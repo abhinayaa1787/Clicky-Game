6c666c55928df47c386243982a1d1e5a38112ed7
Abhinayaa <abhinayaa1787@gmail.com>
2019-07-27 16:39:54 -0400

    Updates

---
+++ precache-manifest.d3c6e49814c7aa63f4a6c5d44af32e11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "316ee45082680f49d914be2eafc5ae58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "215dfae0f33505bde4b6",
+    "url": "/static/css/main.cecc0221.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "215dfae0f33505bde4b6",
+    "url": "/static/js/main.5cde9b32.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
